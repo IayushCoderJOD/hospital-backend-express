2b5511c315459ed4a2f6f28ee960d613942d9b20
ayush tyagi <ayushtyagi4810poco@gmail.com>
2024-03-06 14:59:06 +0530

    backend of hospital game

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const users = [
+  {
+    name: "ayush",
+    kidneys: [
+      {
+        healthy: true,
+      },
+      {
+        healthy: true,
+      },
+      {
+        healthy: false,
+      },
+    ],
+  },
+];
+
+app.get("/", function (req, res) {
+  // will return to user that how many kidney he has and what is the health status of the kidney...
+
+  const ayushKidneys = users[0].kidneys;
+  const numberOfKidney = ayushKidneys.length;
+  let numberOfHealthyKidneys = 0;
+  for (let i = 0; i < ayushKidneys.length; i++) {
+    if (ayushKidneys[i].healthy) {
+      numberOfHealthyKidneys++;
+    }
+  }
+  const numberOfUnhealthyKidneys = numberOfKidney - numberOfHealthyKidneys;
+
+  res.json({
+    ayushKidneys,
+    numberOfHealthyKidneys,
+    numberOfUnhealthyKidneys,
+  });
+});
+
+// to get the body we have to use the below line
+app.use(express.json());
+
+app.post("/", function (req, res) {
+  // want to add new unhealthy or healthy kidney..
+  const isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy,
+  });
+  res.json({
+    msg: "Done!",
+  });
+});
+
+app.put("/", function (req, res) {
+  // replace the unhealthy kidney with the healthy one
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    users[0].kidneys[i].healthy = true;
+  }
+  res.json({
+    msg: "done updation",
+  });
+});
+
+app.delete("/", function (req, res) {
+  // delete the unhealthy kidneys
+  const newKidneys = [];
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    if (users[0].kidneys[i].healthy) {
+      newKidneys.push({
+        healthy: true,
+      });
+    }
+  }
+  users[0].kidneys = newKidneys;
+  res.json({
+    msg: "deleted",
+  });
+});
+
+app.listen(3000);
